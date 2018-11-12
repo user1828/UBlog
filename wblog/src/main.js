d1142e83059e63b5e95d2ab7b7bd1c96943b749d
user1828 <m18438628193@163.com>
2018-11-12 20:43:35 +0800

    刘世涛第三次修改

--- wblog/src/main.js
+++ wblog/src/main.js
@@ -3,8 +3,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from "./store";
+// 引入移动端配置文件
 import "./css/reset.css";
 import "./js/rem.js";
+// 引入icofont图标
+import "../static/wefont/iconfont.css"
+import "../static/wefont/iconfont.js"
 
 Vue.config.productionTip = false
 new Vue({
--- wblog/src/router/index.js
+++ wblog/src/router/index.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/home'
+// import Home from '@/components/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path : "/",
-      redirect :"/home"
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
+    // {
+    //   path : "/",
+    //   redirect :"/home"
+    // },
+    // {
+    //   path: '/home',
+    //   name: 'home',
+    //   component: Home
+    // },
     {
       path:"**",
       component:Home
